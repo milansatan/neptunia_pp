a48a9cef18a392c341996318b1cfe0167eb61b5d
John Asabushi <jasabushi@gmail.com>
2014-11-03 16:30:07 -0800

    'Uploaded'

---
+++ nep_pp/js/common.js
@@ -0,0 +1,40 @@
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-23797156-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+
+
+
+
+function G_Menu(){	
+	document.write(	
+
+		'<li><a href="./">Top</a></li>' +
+		'<li><a href="story.html">Story</a></li>' +
+		'<li><a href="character.html">Characters</a></li>' +
+		'<li><a href="system_mode.html">System</a></li>' +
+		'<li><a href="special.html" class="new">Special</a></li>' +
+		'<li><a href="sound.html">Sound</a></li>' +
+		''
+	);
+};
+
+function G_Menu_in(){	
+	document.write(	
+
+		'<li><a href="../../">Top</a></li>' +
+		'<li><a href="../../story.html">Story</a></li>' +
+		'<li><a href="../../character.html">Characters</a></li>' +
+		'<li><a href="../../system_mode.html">System</a></li>' +
+		'<li><a href="../../special.html" class="new">Special</a></li>' +
+		'<li><a href="../../sound.html">Sound</a></li>' +
+		''
+	);
+};
---
+++ nep_pp/js/header_vita.js
@@ -0,0 +1,4 @@
+$(function() {
+    $("#ifHeader-white").append('<a href="http://us.playstation.com/"><img src="img/psvita.png" alt="PlayStation Vita" id="logo" /></a>')
+});
+// JavaScript Document
